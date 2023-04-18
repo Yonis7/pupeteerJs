6faa10e452dd55ee1536f83c2a43e3518ed19740
Yonis7 <yonisaden7@gmail.com>
2023-04-17 21:13:30 -0400

    Added devtools and slowMo in test

--- tests/example.test.js
+++ tests/example.test.js
@@ -3,7 +3,11 @@ const puppeteer = require('puppeteer')
 describe('My first Pupeteer test', () => {
 
 	it('should launch the browser', async () => {
-		const browser = await puppeteer.launch({headless: false,})
+		const browser = await puppeteer.launch({
+      headless: false,
+       slowMo: 500,
+       devtools: false
+      })
     const page = await browser.newPage()
 
     await page.goto('http://example.com/')
