5db0908685b1501f7057d0a6e18b92557e9f5829
Yonis7 <yonisaden7@gmail.com>
2023-04-20 20:05:09 -0400

    Web scrapper can get names and pics of animals

---
+++ index.js
@@ -0,0 +1,27 @@
+const puppeteer = require('puppeteer')
+const fs = require('fs/promises')
+
+async function start() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://learnwebcode.github.io/practice-requests/');
+  
+  const names = await page.evaluate(() => {
+  return Array.from(document.querySelectorAll(".info strong")).map( name => name.textContent)
+  });
+
+  await fs.writeFile('colors.txt', names.join("\r\n"));
+
+  const photos = await page.$$eval("img", (imgs) => {
+    return imgs.map( x => x.src)
+  });
+
+  for (const currentPhoto of photos) {
+    const imgPage = await page.goto(currentPhoto);
+    await fs.writeFile(currentPhoto.split("/").pop(), await imgPage.buffer())
+
+  }
+
+  await browser.close();
+}
+  start();
